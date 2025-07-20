da19c17cf6d0caafb9f98e8fff5eb39ed448f58e
Abhishek K <abhinavkamble496@gmail.com>
2025-07-20 12:02:34 +0530

    new question

--- InterviewLevelQuestions/BuySellStk.java
+++ InterviewLevelQuestions/BuySellStk.java
@@ -1,8 +1,24 @@
 import java.util.*;
 
 class BuySellStk {
-    
-    public static void main(String[] args){
 
+    public static int BuySell(int price[]){
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i=0; i<price.length;i++){
+            if(buyPrice < price[i]){
+                int profit = price[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } else{
+                buyPrice = price[i];
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args){
+        int price[] = {7,1,5,3,6,4};
+        System.out.println(BuySell(price));
     }
 }
