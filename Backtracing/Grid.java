023c7875fd094f74155feb0b3ebcb5e0a3bdd0eb
Abhishek K <abhinavkamble496@gmail.com>
2025-11-16 13:31:41 +0530

    N Queens

---
+++ Backtracing/Grid.java
@@ -0,0 +1,5 @@
+class Grid{
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Backtracing/Subsets.java
@@ -0,0 +1,20 @@
+class Subsets{
+    public static void findSubsets(String str, String ans, int i) {
+        //base case
+        if(i == str.length()) {
+            System.out.println(ans);
+            return;
+        }
+        //recursion call
+        //yes choice
+        findSubsets(str, ans+str.charAt(i), i+1);
+        //no choice 
+        findSubsets(str, ans, i+1);
+    }
+
+
+    public static void main(String[] args){
+        String str = "abc";
+        findSubsets(str, "", 0);
+    }
+}
