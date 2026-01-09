ffe417a420f0ae967599d4caeb8ea75cb2e6ea62
Abhishek K <abhinavkamble496@gmail.com>
2026-01-10 01:52:29 +0530

    Queue reverse and deque Demo

---
+++ Queues/DequeDemo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class DequeDemo {
+    public static void main(String[] args) {
+        Deque<Integer> d = new LinkedList<>();
+        d.addFirst(1); // 1
+        d.addFirst(2); // 2
+        d.addLast(3);
+        d.addLast(4);
+        System.out.println(d);
+        // d.removeFirst();
+        d.removeLast();
+        System.out.println(d);
+    }
+} 
