5b1484277d97b842eda8038c8f85c1940a11f667
Abhishek K <abhinavkamble496@gmail.com>
2026-01-11 01:00:48 +0530

    Greedy algorithm examples

---
+++ Greedy/ChocolaProblem.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class ChocolaProblem {
+    public static void main(String[] args) {
+        int n = 4 , m = 6;
+
+        Integer cosVer[] = {2, 1, 3, 1, 4}; //m-1
+        Integer cosHor[] = {4, 1, 2}; // n-1
+
+        Arrays.sort(cosVer, Collections.reverseOrder());
+        Arrays.sort(cosHor, Collections.reverseOrder());
+
+        int h = 0, v = 0;
+        int hp = 1, vp = 1;
+        int cost = 0;
+
+        while(h < cosHor.length && v < cosVer.length) {
+            // vertical cost < hor cost
+            if(cosVer[v] <= cosHor[h]) {
+                cost += (cosHor[h] * vp);
+                hp++;
+                h++;
+            } else {
+                cost += (cosVer[v] * hp);
+                vp++;
+                v++;
+            }
+        }
+        while(h < cosHor.length) {
+            cost += (cosHor[h] * vp);
+            hp++;
+            h++;
+        }
+
+        while(v < cosVer.length) {
+            cost += (cosVer[v] * hp);
+            vp++;
+            v++;
+        }
+
+        System.out.println("Min cost of cuts : " + cost);
+    }
+}
