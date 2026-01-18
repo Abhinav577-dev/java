e1211dd5933cc9e280a85e316af645694c97160c
Abhishek K <abhinavkamble496@gmail.com>
2026-01-18 22:34:52 +0530

    New Questions

--- BinaryTree/DiameterOfTree.java
+++ BinaryTree/DiameterOfTree.java
@@ -23,19 +23,42 @@ class DiameterOfTree {
         return Math.max(lh, rh) + 1;
     }
 
-    public static int diam(Node root) {
+    public static int diame(Node root) {
         if(root == null) {
             return 0;
         }
 
-        int leftDiam = diam(root.left);
+        int leftDiam = diame(root.left);
         int leftHt = height(root.left);
-        int rightDiam = diam(root.right);
+        int rightDiam = diame(root.right);
         int rightHt = height(root.right);
 
         int selfDiam = leftHt + rightHt + 1;
 
         return Math.max(selfDiam, Math.max(leftDiam, rightDiam));
+    }
+
+    static class Info {
+        int diam;
+        int ht;
+
+        public Info(int diam, int ht) {
+            this.diam = diam;
+            this.ht = ht;
+        }
+    }
+
+    public static Info diameter(Node root) {
+        if(root == null) {
+            return new Info(0, 0);
+        }
+        Info leftInfo = diameter(root.left);
+        Info rightInfo = diameter(root.right);
+
+        int diam = Math.max(Math.max(leftInfo.diam, rightInfo.diam), leftInfo.ht + rightInfo.ht + 1);
+        int ht = Math.max(leftInfo.ht, rightInfo.ht) + 1;
+
+        return new Info(diam, ht);
     }
 
     public static void main(String[] args) {
@@ -46,6 +69,6 @@ class DiameterOfTree {
         root.left.right = new Node(5);
         root.right.left = new Node(6);
         root.right.right = new Node(7);
-        System.out.println(diam(root));
+        System.out.println(diameter(root).diam);
     }
 }
