156ed4e18899f0e7974c20b6b4ed19b461a36522
Abhishek K <abhinavkamble496@gmail.com>
2026-01-20 15:43:45 +0530

    New examples

--- BinaryTree/TopViewTree.java
+++ BinaryTree/TopViewTree.java
@@ -76,6 +76,69 @@ class TopViewTree {
         kLevel(root.right, level+1, k);
     }
 
+    public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+        if(root == null) {
+            return false;
+        }
+        path.add(root);
+        if(root.data == n) {
+            return true;
+        }
+
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+
+        if(foundLeft || foundRight) {
+            return true;
+        }
+
+        path.remove(path.size() - 1);
+        return false;
+    }
+
+    public static Node lca(Node root, int n1, int n2) {
+        ArrayList<Node> p1 = new ArrayList<>();
+        ArrayList<Node> p2 = new ArrayList<>();
+
+        getPath(root, n1, p1);
+        getPath(root, n2, p2);
+
+        int i=0;
+        for(i=0; i<p1.size() && i<p2.size(); i++) {
+            if(p1.get(i) != p2.get(i)) {
+                break;
+            }
+        }
+
+        //last equal node -> i-1th 
+
+
+        Node lca = p1.get(i-1);
+        return lca;
+    }
+
+    public static Node lca2(Node root, int n1, int n2) {
+        
+        if(root == null || root.data == n1 || root.data == n2){
+            return root;
+        }
+
+        Node leftlca = lca2(root.left, n1, n2);
+        Node rightlca = lca2(root.right, n1, n2);
+
+        //left  = val right = null
+
+        if(rightlca == null) {
+            return leftlca;
+        }
+
+        if(leftlca == null) {
+            return rightlca;
+        }
+
+        return root;
+    }
+
     public static void main(String[] args) {
         Node root = new Node(1);
         root.left = new Node(2);
@@ -86,7 +149,8 @@ class TopViewTree {
         root.right.right = new Node(7);
 
         //topView(root);
-        int k = 2;
-        kLevel(root, 1, k);
+        // int k = 2;
+        // kLevel(root, 1, k);
+        System.out.println(lca2(root, 4, 5).data);
     }
 }
