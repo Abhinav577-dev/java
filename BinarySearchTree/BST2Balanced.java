9f39f1819ab24a47511bd0e722f0e58070803a22
Abhishek K <abhinavkamble496@gmail.com>
2026-01-25 23:38:01 +0530

    BST 2 Balanaced bst

---
+++ BinarySearchTree/BST2Balanced.java
@@ -0,0 +1,15 @@
+class BST2Balanced {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node (int data) {
+            this.data = data;
+        }
+    }
+    public static Node createBST(ArrayList<Integer> list)
+    public static void main(String[] args) {
+
+    }
+}
