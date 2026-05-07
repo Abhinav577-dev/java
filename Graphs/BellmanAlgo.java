4a5b440df958bdab384841c2841b7a01b8a40014
Abhishek K <abhinavkamble496@gmail.com>
2026-05-07 20:57:16 +0530

    Bellman Ford Algorithm

---
+++ Graphs/BellmanAlgo.java
@@ -0,0 +1,120 @@
+import java.util.*;
+class BellmanAlgo {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge> graph[]) {
+        for(int i=0; i<graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        //0 vertex
+        graph[0].add(new Edge(0, 1, 2));
+        graph[0].add(new Edge(0, 2, 4)); 
+
+        graph[1].add(new Edge(1, 3, 7));
+        graph[1].add(new Edge(1, 2, 1));   
+
+        graph[2].add(new Edge(2, 4, 3));
+        
+
+        //3 vertex
+        graph[3].add(new Edge(3, 5, 1));
+
+        graph[4].add(new Edge(4, 3, 2));
+        graph[4].add(new Edge(4, 5, 5));
+
+    }
+
+    static class Pair implements Comparable<Pair> {
+        int v;
+        int cost;
+
+        public Pair(int v, int cost) {
+            this.v = v;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Pair p2) {
+            return this.cost - p2.cost;
+        }
+    }
+
+    public static void bellmanFord(ArrayList<Edge> graph[], int src) {
+        int dist[] = new int[graph.length];
+        for(int i=0; i<dist.length; i++) {
+            if(i != src) {
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+
+        int V = graph.length;
+
+        for(int i=0; i<V-1; i++) {
+            //edges O(E)
+            for(int j=0; j<graph.length; j++) {
+                for(int k=0; k<graph[j].size(); k++) {
+                    Edge e = graph[j].get(k);
+                    // u, v, w
+                    int u = e.src;
+                    int v = e.dest;
+                    int wt = e.wt;
+
+                    //relaxation
+                    if(dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v]) {
+                        dist[v] = dist[u] + wt;
+                    }
+                }
+            }
+        }
+
+        for(int i=0; i<dist.length; i++) {
+            System.out.print(dist[i] + " ");
+        }
+
+        System.out.println();
+    }
+
+    // public static void prims(ArrayList<Edge> graph[]) {
+    //     boolean vis[] = new boolean[graph.length];
+    //     PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+    //     pq.add(new Pair(0, 0));
+
+    //     int finalCost= 0;
+
+    //     while(!pq.isEmpty()) {
+    //         Pair curr = pq.remove();
+    //         if(!vis[curr.v]) {
+    //             vis[curr.v] = true;
+    //             finalCost += curr.cost;
+
+    //             for(int i=0; i<graph[curr.v].size(); i++) {
+    //                 Edge e = graph[curr.v].get(i);
+    //                 pq.add(new Pair(e.dest, e.wt));
+    //             }
+    //         }
+    //     }
+
+    //     System.out.println("final cost of MST = "+ finalCost);
+    // }
+
+
+    public static void main(String[] args) {
+        int V = 6;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);  
+        //prims(graph);   
+        bellmanFord(graph, 0);  
+
+    }
+}
