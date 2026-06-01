dc79696f481e2fd1824f7052d1c4dbf68d43e171
Abhishek K <abhinavkamble496@gmail.com>
2026-06-01 15:22:12 +0530

    tabulation dp

--- DP/Revision.java
+++ DP/Revision.java
@@ -9,9 +9,18 @@ class Revision {
         f[n] = fib(n-1, f) + fib(n-2, f);
         return f[n];
     }
+    public static int fibTab(int n) {
+        int dp[] = new int[n+1];
+        dp[1] = 1;
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
     public static void main(String[] args) {
         int n = 5;
-        int f[] = new int[n+1];
-         System.out.println(fib(n, f));
+        // int f[] = new int[n+1];
+        //  System.out.println(fib(n, f));
+        System.out.println(fibTab(n));
     }
 }
