feadb3166139362a6253c8f3332f3365ff5f13c9
Abhishek K <abhinavkamble496@gmail.com>
2026-06-02 20:37:44 +0530

    Rod cutting problem

---
+++ DP/RodProb.java
@@ -0,0 +1,42 @@
+class RodProb {
+
+    public static int rodCutting(int length[], int prices[], int totRod) {
+        int n = prices.length;
+        int dp[][] = new int[n+1][totRod+1];
+
+        for(int i=0; i<n+1; i++) {
+            dp[i][0] = 0;
+        }
+
+        for(int j=0; j<totRod+1; j++) {
+            dp[0][j] = 0;
+        }
+
+        for(int i=1; i<n+1; i++) {
+            for(int j=1; j<totRod+1; j++) {
+                if(length[i-1] <= j) {
+                    dp[i][j] = Math.max(prices[i-1] + dp[i][j-length[i-1]] , dp[i-1][j]);
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        print(dp);
+        return dp[n][totRod];
+    }
+    public static void print(int dp[][]) {
+        for(int i=0; i<dp.length; i++) {
+            for(int j=0; j<dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int length[] = {1, 2, 3, 4, 5, 6, 7, 8};
+        int prices[] = {1, 5, 8, 9, 10, 17, 17, 20};
+        int totRod = 8;
+        System.out.println(rodCutting(length, prices, totRod));
+    }
+}
