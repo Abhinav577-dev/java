bc68bf51792b5b4abb3aeee39ee3bf5bad31841b
Abhishek K <abhinavkamble496@gmail.com>
2026-06-03 15:31:13 +0530

    Longest common subsequence problem

---
+++ DP/LCSubStr.java
@@ -0,0 +1,45 @@
+class LCSubStr {
+    public static int lcs(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+
+        int ans = Integer.MIN_VALUE;
+
+        int dp[][] = new int[n+1][m+1];
+        for(int i=0; i<n+1; i++) {
+            for(int j=0; j<m+1; j++) {
+                if(i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        for(int i=1; i<n+1; i++) {
+            for(int j=1; j<m+1; j++) {
+                if(str1.charAt(i-1) == str2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                    ans = Math.max(ans, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        print(dp);
+        return ans;
+    }
+
+    public static void print(int dp[][]) {
+        for(int i=0; i<dp.length; i++) {
+            for(int j=0; j<dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        String str1 = "ABCDE";
+        String str2 = "ABGCE";
+        System.out.println(lcs(str1, str2));
+    }
+}
